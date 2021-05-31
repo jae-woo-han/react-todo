a775bb66ddebb1c707187fcd2022641fa0327193
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-06-01 00:17:11 +0900

    prit

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -12,7 +12,7 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
 
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={()=>onToggle(id)}>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
