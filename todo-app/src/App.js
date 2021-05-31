5e08b5dc9bd833b0c7fcb1af7e2a46907a8d2825
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-05-31 23:56:25 +0900

    Remove List Item

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -22,12 +21,35 @@ const App = () => {
       checked: false,
     },
   ]);
+
+  // 고유값으로 사용 될 id
+  // ref 를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+  const onRemove = useCallback(
+    id=> {
+      setTodos(todos.filter(todo=> todo.id !==id));
+    },
+    [todos],
+  )
   
   return (
-  <TodoTemplate>
-    <TodoInsert />
-    <TodoList todos={todos} />
-  </TodoTemplate>
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} />
+    </TodoTemplate>
   );
 };
 
