a3b5cc4bd7a6be97e9d50d168fc5ab8b16d82b9b
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-12-27 23:19:16 +0900

    feat 구매가능 수 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [cash, setCash] = useState(0);
+  const [selectedCoinPrice, setSelectedCoinPrice] = useState(0);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((res) => res.json())
@@ -11,19 +13,31 @@ function App() {
         setLoading(false);
       });
   }, []);
+  const onChageCash = (event) => {
+    setCash(event.target.value);
+  };
+  const onChangeCoin = (event) => {
+    console.log(event.target.value);
+    setSelectedCoinPrice(event.target.value);
+  };
   return (
     <div>
       <h1>The Coins!({coins.length})</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
+        <div>
+          <input type="text" onChange={onChageCash} value={cash} />
+
+          <select onChange={onChangeCoin}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <div>{Math.floor(cash / selectedCoinPrice)}</div>
+        </div>
       )}
     </div>
   );
