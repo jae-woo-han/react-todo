cf36b01dd74ca195613a2d8502ce31ddd141bb61
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-12-27 22:38:43 +0900

    feat fetch coinpaprika

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>The Coins!</h1>
+      <h1>The Coins!({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
