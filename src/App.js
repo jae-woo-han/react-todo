bedae87e1a43a6be214649c833d3cadd1a44e819
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-12-27 22:18:11 +0900

    feat start coinTracker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChage = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArr) => [toDo, ...currentArr]);
-  };
+  const [loading, setLoading] = useState(true);
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChage}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins!</h1>
     </div>
   );
 }
