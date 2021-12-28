17378ab24aca5fb846c1202f36915b03ca29ed6c
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-12-28 22:18:24 +0900

    style shorter await

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const res = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    );
-    const json = await res.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
