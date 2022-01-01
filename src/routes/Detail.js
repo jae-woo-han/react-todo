4f16c75a3cdb689ce9b0b9e851fb7afb8c875b10
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-01-01 22:13:07 +0900

    feat movie detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
+
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
 
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
+    console.log(json.data.movie);
   };
 
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      <h1>Detail</h1>
+      <div>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div>
+            <Movie
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.description_full}
+              genres={movie.genres}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
