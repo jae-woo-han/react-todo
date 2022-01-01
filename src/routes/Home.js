669b6e44f215872437b3aa24da120a175fbe4894
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-01-01 22:05:11 +0900

    style document format

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 
-function Home(){
-    const [loading, setLoading] = useState(true);
+function Home() {
+  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
     const json = await (
@@ -27,6 +27,7 @@ function Home(){
           {movies.map((movie) => (
             <Movie
               key={movie.id}
+              id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
